2fa687667f132bd962665b9a3ac546dde420816d
jujusoi <jalxmcdonald@hotmail.com>
2023-11-09 08:43:58 +1030

    setup base erver

--- server/server.js
+++ server/server.js
@@ -9,4 +9,29 @@ const db = require('./config/connection');
 const PORT = process.env.PORT || 4000;
 const app = express();
 
-//set up server tomorrow
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    persistedQueries: false,
+});
+
+const startApolloServer = async () => {
+    await server.start();
+    app.use(express.urlencoded({ extended: false }));
+    app.use(express.json());
+    app.use('/graphql', expressMiddleware(server));
+    if (process.env.NODE_ENV === 'production') {
+        app.use(express.static(path.join(__dirname, '../client/dist')));
+        app.get('*', (req, res) => {
+            res.sendFile(path.join(__dirname, '..client/dist/index.html'));
+        });
+    };
+    db.once('open', () => {
+        app.listen(PORT, () => {
+            console.log(`Server up at http://localhost:${PORT}`);
+            console.log(`GraphQL up at http://localhost:${PORT}/graphql`)
+        });
+    });
+};
+
+startApolloServer();
