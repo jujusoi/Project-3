d93c91ad3e44f8418d3d9506c9cc9ce36e21f5aa
jujusoi <jalxmcdonald@hotmail.com>
2023-11-09 08:55:48 +1030

    finished base typedef

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,3 +1,23 @@
 const typeDefs = `
+    type Profile {
+        _id: ID!,
+        email: String!,
+        password: String!,
+        firstName: String,
+        lastName: String,
+        orgName: String,
+        age: Integer,
+        userLocation: String!,
+        experience: String,
+        biography: String!,
+        industry: String!,
+        profilePicture: String,
+        isOrganisation: Boolean!,
+    }
 
+    type Query {
+        profiles: [Profile]
+    }
 `
+
+module.exports = typeDefs;
