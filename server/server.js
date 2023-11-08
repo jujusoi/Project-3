4a62a2bd9ac84760f1c44d3e4844c10c551c0924
jujusoi <jalxmcdonald@hotmail.com>
2023-11-08 23:24:32 +1030

    finished base server setup

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const { ApolloServer } = require('@apollo/server');
+const { expressMiddleware } = require('@apollo/server/express4');
+const path = require('path');
+
+const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
+
+const PORT = process.env.PORT || 4000;
+const app = express();
+
+//set up server tomorrow
