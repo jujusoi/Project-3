3ecdff3570d0d7a35ba941d1373b4dcca2bfa486
jujusoi <jalxmcdonald@hotmail.com>
2023-11-09 19:25:52 +1030

    added auth middleware

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -3,6 +3,22 @@ const secret = 'omaygottamattaa';
 const expiration = '3h';
 
 module.exports = {
+    authMiddleware: function (req, res, next) {
+        let token = req.query.token || req.headers.authorization;
+        if (req.headers.authorization) {
+            token = token.split(' ').pop().trim();
+        };
+        if (!token) {
+            return res.status(400).json({message: 'Token not found'});
+        };
+        try {
+            const { data } = jwt.verify(token, secret, { maxAge: expiration });
+            req.profile = data;
+        } catch {
+            console.log('Invalid token');
+            return res.status(400).json({ message: 'Invalid Token' });
+        }
+    },
     signToken: function (userInfo) {
         const payload = { userInfo };
         return jwt.sign({ data: payload }, secret, { expiresIn: expiration});
