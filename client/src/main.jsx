a015be571b5ae718bc70a67d6a4bca3e90a070a1
jujusoi <jalxmcdonald@hotmail.com>
2023-11-09 19:53:40 +1030

    serverized the client

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
 import App from './App.jsx'
 import './index.css'
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <h2>404 Not Found!!!</h2>,
+    children: [
+      {
+        index: true,
+        element: <h2>Main page (Job area)</h2>,
+      }, {
+        path: '/profile',
+        element: <h2>This is a user's profile</h2>
+      },
+    ],
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  <RouterProvider router={router} />
+);
