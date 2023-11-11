d6f00f872ac2e7df39bf8f84c3409e375960cb10
jujusoi <jalxmcdonald@hotmail.com>
2023-11-11 21:41:51 +1030

    attempted to add time conversion

--- client/src/components/listingComps/miniListings.jsx
+++ client/src/components/listingComps/miniListings.jsx
@@ -8,6 +8,8 @@ export default function MiniListings({ orgName, index }) {
         variables: {orgName},
     });
 
+    console.log(data);
+
     if (loading) {
         return (
             <h3>Loading...</h3>
@@ -23,7 +25,7 @@ export default function MiniListings({ orgName, index }) {
                                                 <Link to={`/listing/${data.listingByOrganisation[index]._id}`} target="_blank"><h4 style={{ textAlign: 'left', marginTop: 0, marginBottom: 5}}  className="listing-title">{data.listingByOrganisation[index].title}</h4></Link>
                                                 <p style={{ textAlign: 'left' }} className="listing-salary">{data.listingByOrganisation[index].salary}</p>
                                             </div>
-                                            <p className="listing-date" style={{ width:'30%', marginTop: 0, textAlign: "right"}}>10th Nov, 2023</p>
+                                            <p className="listing-date" style={{ width:'30%', marginTop: 0, textAlign: "right"}}>{() => convertTime(data.listingByOrganisation[index].postedOn)}</p>
                                         </div>
                                         <div className="ilj-o-hold" style={{ display: "flex", flexDirection: 'column', justifyContent: 'space-between', alignItems: 'center'}}>
                                             <div className="liilj-hold" style={{ width: '100%', justifyContent: 'space-between'}}>
