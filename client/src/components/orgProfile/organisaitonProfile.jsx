8405a243c763a47acbdc852cd2bf4d98774dcc61
jujusoi <jalxmcdonald@hotmail.com>
2023-11-12 18:06:13 +1030

    added individual user profile

--- client/src/components/orgProfile/organisaitonProfile.jsx
+++ client/src/components/orgProfile/organisaitonProfile.jsx
@@ -26,15 +26,16 @@ export default function OrganisationProfile({ profileData }) {
                 <section id="org-profile-sect" style={{ width: '90%', margin: 'auto'}}>
                     <div id="org-profile-holder" style={{ marginTop: 40 }}>
                         <div id="org-tpi-hold" style={{ display: "flex", alignItems: 'center'}}>
-                            <div id="org-pfp"><img width={100} height={100} src="https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcR8Oytq1aOdKp8GpVdNTLPN2-rl_uq4bft4_TjG9WUzvtT2-WJlQXZFOuzpmklzm2qCE4qUTnjLNe971ys" style={{ borderRadius: 50}}></img></div>
+                            <div id="org-pfp"><img width={100} height={100} src={profileData.profilePicture} style={{ borderRadius: 50}}></img></div>
                             <div id="org-title-ind" style={{ textAlign: "left", marginLeft: 28 }}>
                                 <h2>{profileData.orgName}</h2>
-                                <p>{profileData.industry}</p>
+                                <p>Industry: <b>{profileData.industry}</b></p>
                             </div>
                         </div>
                         <div id="org-lbc-hold" style={{ display: "flex", flexDirection: 'column', textAlign: "left", padding: 35}}>
-                            <div id="org-loc">
-                                <h4>Location: {profileData.userLocation}</h4>
+                            <div id="org-loc" style={{ display: 'flex', alignItems: 'center', marginBottom: 15 }}>
+                                <h4 style={{ marginBottom: 0, marginRight: 12}}>Location: </h4>
+                                <p style={{ marginBottom: 0}}> {profileData.userLocation}</p>
                             </div>
                             <div id="org-bio">
                                 <h4>Biography:</h4>
--- client/src/components/userProfile/userProfile.jsx
+++ client/src/components/userProfile/userProfile.jsx
@@ -3,7 +3,35 @@
 export default function NormalUserProfile({ profileData }) {
     return (
         <>
-            <h3>{profileData.firstName} {profileData.lastName}</h3>
+ <section id="org-profile-sect" style={{ width: '90%', margin: 'auto'}}>
+                    <div id="org-profile-holder" style={{ marginTop: 40 }}>
+                        <div id="org-tpi-hold" style={{ display: "flex", alignItems: 'center'}}>
+                            <div id="org-pfp"><img width={100} height={100} src={profileData.profilePicture} style={{ borderRadius: 50}}></img></div>
+                            <div id="org-title-ind" style={{ textAlign: "left", marginLeft: 28 }}>
+                                <h2>{profileData.firstName} {profileData.lastName}</h2>
+                                <p>Looking for a job in: <b>{profileData.industry}  </b></p>
+                            </div>
+                        </div>
+                        <div id="org-lbc-hold" style={{ display: "flex", flexDirection: 'column', textAlign: "left", padding: 35}}>
+                            <div id="org-loc" style={{ display: 'flex', alignItems: 'center', marginBottom: 15 }}>
+                                <h4 style={{ marginBottom: 0, marginRight: 12}}>Location: </h4>
+                                <p style={{ marginBottom: 0}}> {profileData.userLocation}</p>
+                            </div>
+                            <div id="org-bio">
+                                <h4>Biography:</h4>
+                                <p>{profileData.biography}</p>
+                            </div>
+                            <div id="org-exp">
+                                <h4>Experience:</h4>
+                                <p>{profileData.experience}</p>
+                            </div>
+                            <div id="org-contact">
+                                <h4>{profileData.firstName} {profileData.lastName[profileData.lastName.length - 1] == 's' ? profileData.lastName + `'` : profileData.lastName + `'s`} Contacts:</h4>
+                                <p>Email: {profileData.email}</p>
+                            </div>
+                        </div>
+                    </div>
+                </section>
         </>
     );
 }
