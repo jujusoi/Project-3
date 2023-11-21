746a001a67a08279a5a40122ddf73995e3bf35f8
jujusoi <jalxmcdonald@hotmail.com>
2023-11-21 15:34:06 +1030

    d

--- server/server.js
+++ server/server.js
@@ -23,9 +23,9 @@ const startApolloServer = async () => {
     app.use(express.json());
     app.use('/graphql', expressMiddleware(server));
     if (process.env.NODE_ENV === 'production') {
-        app.use(express.static(path.join(__dirname, '../client/build')));
+        app.use(express.static(path.join(__dirname, '../client/dist')));
         app.get('*', (req, res) => {
-            res.sendFile(path.join(__dirname, '..client/build/index.html'));
+            res.sendFile(path.join(__dirname, '..client/dist/index.html'));
         });
     };
     db.once('open', () => {
